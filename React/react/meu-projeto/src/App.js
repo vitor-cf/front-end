853ccaa7391b49e16360932cc20d17a7aa4eeceb
Vitor <vitorcarvalho.dev@gmail.com>
2022-10-19 22:53:00 -0300

    finalizando teoria react

--- React/react/meu-projeto/src/App.js
+++ React/react/meu-projeto/src/App.js
@@ -2,7 +2,7 @@ import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import Home from './pages/Home'
 import Contato from './pages/Contato'
 import Empresa from './pages/Empresa'
-import NavBar from './components/NavBar'
+import NavBar from './components/layout/NavBar'
 import Footer from './components/layout/Footer'
 
 function App() {
